c279808c2cc9fe6ccb317ad7424ce7f9ec5542fd
The Doctor <drwho@virtadpt.net>
2014-03-02 21:19:38 -0800

    Copied the working testbot over to exocortex.py.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- exocortex.py
+++ exocortex.py
@@ -47,6 +47,7 @@
 # Load modules.
 import ConfigParser
 import json
+from optparse import OptionParser
 import os
 import random
 import resource
@@ -451,6 +452,25 @@ def process_status(botname):
     status = status + "The current system load is %s." % str(os.getloadavg())
     return status
 
+""" Figure out what to set the logging level to.  There isn't a
+straightforward way of doing this because Python uses constants that are
+actually integers under the hood, and I'd really like to be able to do
+something like loglevel = 'logging.' + loglevel.  I can't have a pony,
+either.  Takes a string, returns a Python loglevel. """
+def process_loglevel(loglevel):
+    if loglevel == 'critical':
+        return 50
+    if loglevel == 'error':
+        return 40
+    if loglevel == 'warning':
+        return 30
+    if loglevel == 'info':
+        return 20
+    if loglevel == 'debug':
+        return 10
+    if loglevel == 'notset':
+        return 0
+
 # Core code...
 if __name__ == '__main__':
     # If we're running in a Python environment earlier than v3.0, set the
@@ -463,12 +483,34 @@ if __name__ == '__main__':
     # extension, if there is one).
     botname = os.path.basename(__file__).split('.')[0]
 
-    # Read its unique configuration file.  Do this by taking the name of the
-    # bot and appending '.conf' to it.  Then load it into a config file parser
-    # object which has some defaults set on it.
-    config = ConfigParser.ConfigParser()
-    config .read(botname + '.conf')
+    # Instantiate a command line options parser.
+    optionparser = OptionParser()
 
+    # Define command line switches for the bot, starting with being able to
+    # specify an arbitrary configuration file for a particular bot.
+    optionparser.add_option('-c', '--conf', dest='configfile', action='store',
+        type='string', help='Specify an arbitrary config file for this bot.  Defaults to botname.conf.')
+
+    # Add a command line option that lets you override the config file's
+    # loglevel.  This is for kicking a bot into debug mode without having to
+    # edit the config file.
+    optionparser.add_option('-l', '--loglevel', dest='loglevel', action='store',
+        help='Specify the default logging level of the bot.  Choose from CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET.  Defaults to INFO.')
+
+    # Parse the command line args.
+    (options, args) = optionparser.parse_args()
+
+    # Read its unique configuration file.  There is a command line argument
+    # for specifying a configuration file, but it default to taking the name
+    # of the bot and appending '.conf' to it.  Then load it into a config file
+    # parser object.
+    config = ConfigParser.ConfigParser()
+    if options.configfile:
+        config.read(options.configfile)
+    else:
+        config.read(botname + '.conf')
+
+    # Get configuration options out of whichever configuration file gets used.
     owner = config.get(botname, 'owner')
     username = config.get(botname, 'username')
     password = config.get(botname, 'password')
@@ -477,8 +519,19 @@ if __name__ == '__main__':
     imalive = config.get(botname, 'imalive')
     function = config.get(botname, 'function')
 
-    # Figure out how to configure the logger.
-    loglevel = config.get(botname, 'loglevel')
+    # Figure out how to configure the logger.  Start by reading the config
+    # file.
+    config_log = config.get(botname, 'loglevel').lower()
+    if config_log:
+        loglevel = process_loglevel(config_log)
+
+    # Then try the command line.
+    if options.loglevel:
+        loglevel = process_loglevel(options.loglevel.lower())
+
+    # Default to WARNING.
+    if not options.loglevel and not loglevel:
+        loglevel = logging.WARNING
 
     # Get the filename of the response file from the config file.
     responsefile = config.get(botname, 'responsefile')
@@ -486,8 +539,8 @@ if __name__ == '__main__':
     # Log into the XMPP server.  If it can't log in, try to register an account
     # with the server.  If it's a private server this shouldn't be a problem,
     # else print an error to stderr and ABEND.
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(levelname)-8s %(message)s')
+    logging.basicConfig(level=loglevel,
+        format='%(levelname)-8s %(message)s')
     bot = ExocortexBot(owner, botname, username, password, muc, muclogin,
         imalive, responsefile, function)
 
@@ -497,8 +550,6 @@ if __name__ == '__main__':
     else:
         print "ERROR: Unable to connect to XMPP server."
         sys.exit(1)
-
-# Clean up after ourselves.
 
 # Fin.
 
